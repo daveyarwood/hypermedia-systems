3b875a74b21018c4c0cb611a9a5434f959f84d88
Dave Yarwood <dave.yarwood@gmail.com>
2026-05-16 16:45:34 -0400

    Alpine.js counter

--- resources/public/js/counter.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll("[data-counter]").forEach((el) => {
-  const output = el.querySelector("[data-counter-output]");
-  const increment = el.querySelector("[data-counter-increment]");
-
-  increment.addEventListener("click", (_e) => {
-    output.textContent++;
-  });
-});
