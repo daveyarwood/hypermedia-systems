5a5c31ed444349d1b847afb4b9c91eb8f3ce9650
Dave Yarwood <dave.yarwood@gmail.com>
2026-05-23 19:08:55 -0400

    SweetAlert2 event-based attempt
    The alert dialog opens and `isConfirmed` is true, but then when it goes to
    dispatch the event, this error happens for some reason:
    
    > missing ] after element list
    
    and only when I'm trying to debug the event. Maybe a bug in the Firefox
    debugger?

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -74,3 +74,11 @@ const overflowMenu = (tree = document) => {
 };
 
 addEventListener("htmx:load", (e) => overflowMenu(e.target));
+
+window.sweetConfirm = (el, config) => {
+  Swal.fire(config).then(({ isConfirmed }) => {
+    if (isConfirmed) {
+      el.dispatchEvent(new Event("confirmed"));
+    }
+  });
+};
